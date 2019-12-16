e3907ab9adcfc53d9dbcd8f8b7aa03c584aa2d05
Eugene Mishura <eugene@tqgroup.io>
2019-12-16 14:31:59 -0500

    added src ligo files to python test package

--- multi_asset/setup.py
+++ multi_asset/setup.py
@@ -6,7 +6,8 @@ setup(
     name="tezos-mac-tests",
     version="1.0",
     description="Multi Asset Contract Tests",
-    packages=["test"],
-    install_requires=["pytezos"]
+    packages=["mactest"],
+    install_requires=["pytezos"],
+    include_package_data=True,
 )
 
