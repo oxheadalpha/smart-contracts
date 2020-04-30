1233abd57289d27f7b1115340966963f76f999a0
Eugene Mishura <eugene@tqgroup.io>
2020-04-30 16:39:24 -0400

    copied single to nft

---
+++ nft/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="tezos_fa2_single_tests",
+    version="1.0",
+    description="FA2 Single Asset Contract Tests",
+    packages=["tezos_fa2_single_tests"],
+    install_requires=["pysodium", "secp256k1", "fastecdsa", "pytezos", "crypto"],
+    include_package_data=True,
+)
+
