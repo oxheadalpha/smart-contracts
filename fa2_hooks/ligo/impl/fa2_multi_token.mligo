d72479b7191d1c040624512986f6a561a23a2bd2
Eugene Mishura <eugene@tqgroup.io>
2020-06-18 11:21:10 -0400

    initial setup for fa2 MAC with hooks

---
+++ fa2_hooks/ligo/impl/fa2_multi_token.mligo
@@ -0,0 +1,122 @@
+#if !FA2_MAC_TOKEN
+#define FA2_MAC_TOKEN
+
+#include "../fa2_interface.mligo"
+#include "../fa2_errors.mligo"
+#include "../lib/fa2_operator_lib.mligo"
+
+(* (owner,token_id) -> balance *)
+type ledger = ((address * token_id), nat) big_map
+
+(* token_id -> total_supply *)
+type token_total_supply = (token_id, nat) big_map
+
+(* token_id -> token_metadata *)
+type token_metadata_storage = (token_id, token_metadata_michelson) big_map
+
+type multi_token_storage = {
+  ledger : ledger;
+  operators : operator_storage;
+  token_total_supply : token_total_supply;
+  token_metadata : token_metadata_storage;
+}
+
+let get_balance_amt (key, ledger : (address * nat) * ledger) : nat =
+  let bal_opt = Big_map.find_opt key ledger in
+  match bal_opt with
+  | None -> 0n
+  | Some b -> b
+
+let inc_balance (owner, token_id, amt, ledger
+    : address * token_id * nat * ledger) : ledger =
+  let key = owner, token_id in
+  let bal = get_balance_amt (key, ledger) in
+  let updated_bal = bal + amt in
+  Big_map.update key (Some updated_bal) ledger 
+
+let dec_balance (owner, token_id, amt, ledger
+    : address * token_id * nat * ledger) : ledger =
+  let key = owner, token_id in
+  let bal = get_balance_amt (key, ledger) in
+  match Michelson.is_nat (bal - amt) with
+  | None -> (failwith fa2_insufficient_balance : ledger)
+  | Some new_bal ->
+    if new_bal = 0n
+    then Big_map.remove key ledger
+    else Map.update key (Some new_bal) ledger
+
+(**
+Update leger balances according to the specified transfers. Fails if any of the
+permissions or constraints are violated.
+@param txs transfers to be applied to the ledger
+@param owner_validator function that validates of the tokens from the particular owner can be transferred. 
+ *)
+let transfer (txs, owner_validator, storage
+    : (transfer list) * ((address * operator_storage) -> unit) * multi_token_storage)
+    : ledger =
+  let make_transfer = fun (l, tx : ledger * transfer) ->
+    let u = owner_validator (tx.from_, storage.operators) in
+    List.fold 
+      (fun (ll, dst : ledger * transfer_destination) ->
+        if not Big_map.mem dst.token_id storage.token_metadata
+        then (failwith fa2_token_undefined : ledger)
+        else
+          let lll = dec_balance (tx.from_, dst.token_id, dst.amount, ll) in
+          inc_balance(dst.to_, dst.token_id, dst.amount, lll)
+      ) tx.txs l
+  in
+  List.fold make_transfer txs storage.ledger
+ 
+let get_balance (p, ledger, tokens
+    : balance_of_param * ledger * token_total_supply) : operation =
+  let to_balance = fun (r : balance_of_request) ->
+    if not Big_map.mem r.token_id tokens
+    then (failwith fa2_token_undefined : balance_of_response_michelson)
+    else
+      let key = r.owner, r.token_id in
+      let bal = get_balance_amt (key, ledger) in
+      let response = { request = r; balance = bal; } in
+      balance_of_response_to_michelson response
+  in
+  let responses = List.map to_balance p.requests in
+  Operation.transaction responses 0mutez p.callback
+
+
+let fa2_main (param, storage : fa2_entry_points * multi_token_storage)
+    : (operation  list) * multi_token_storage =
+  match param with
+  | Transfer txs_michelson -> 
+     (* convert transfer batch into `transfer_descriptor` batch *)
+    let txs = transfers_from_michelson txs_michelson in
+    (* 
+    will validate that a sender is either `from_` parameter of each transfer
+    or a permitted operator for the owner `from_` address.
+    *)
+    let validator = make_default_operator_validator Tezos.sender in
+    let new_ledger = transfer (txs, validator, storage) in
+    let new_storage = { storage with ledger = new_ledger; }
+    in ([] : operation list), new_storage
+
+  | Balance_of pm -> 
+    let p = balance_of_param_from_michelson pm in
+    let op = get_balance (p, storage.ledger, storage.token_total_supply) in
+    [op], storage
+
+  | Update_operators updates_michelson ->
+    let updates = operator_updates_from_michelson updates_michelson in
+    let updater = Tezos.sender in
+    let process_update = (fun (ops, update : operator_storage * update_operator) ->
+      let u = validate_update_operators_by_owner (update, updater) in
+      update_operators (update, ops)
+    ) in
+    let new_ops =
+      List.fold process_update updates storage.operators in
+    let new_storage = { storage with operators = new_ops; } in
+    ([] : operation list), new_storage
+
+  | Token_metadata_registry callback ->
+    (* the contract maintains token metadata in its storage - `token_metadata` big_map *)
+    let callback_op = Operation.transaction Tezos.self_address 0mutez callback in
+    [callback_op], storage
+
+#endif
---
+++ fa2_hooks/tezos_fa2_hooks_tests/test_mac.py
@@ -0,0 +1,233 @@
+from pathlib import Path
+from decimal import *
+from unittest import TestCase
+
+from pytezos import Key, pytezos
+
+from tezos_mac_tests.ligo import (
+    LigoEnv,
+    LigoContract,
+    PtzUtils,
+    flextesa_sandbox,
+)
+
+
+root_dir = Path(__file__).parent.parent / "ligo"
+ligo_env = LigoEnv(root_dir / "impl", root_dir / "out")
+
+
+class TestMacSetUp(TestCase):
+    def setUp(self):
+        self.util = PtzUtils(flextesa_sandbox)
+        # self.util = PtzUtils(pytezos)
+
+        self.admin_key = self.util.client.key
+
+        self.orig_contracts()
+
+        self.mike_key = Key.generate(export=False)
+        self.kyle_key = Key.generate(export=False)
+
+        # self.transfer_init_funds()
+        print("test setup completed")
+
+    def orig_contracts(self):
+        print("loading ligo contracts...")
+        ligo_fa2 = ligo_env.contract_from_file(
+            "fa2_multi_asset.mligo", "multi_asset_main"
+        )
+        ligo_receiver = ligo_env.contract_from_file("token_owner.mligo", "main")
+        ligo_inspector = ligo_env.contract_from_file("inspector.mligo", "main")
+
+        print("originating contracts...")
+        self.fa2 = self.orig_mac(ligo_fa2)
+        print(f"FA2 address {self.fa2.address}")
+
+        self.alice_receiver = self.orig_receiver(ligo_receiver)
+        print(f"Alice address {self.alice_receiver.address}")
+        self.bob_receiver = self.orig_receiver(ligo_receiver)
+        print(f"Bob address {self.bob_receiver.address}")
+        self.inspector = self.orig_inspector(ligo_inspector)
+
+    def orig_mac(self, ligo_mac):
+
+        ligo_storage = (
+            """
+        {
+            admin = {
+                admin = ("%s" : address);
+                pending_admin = (None : address option);
+                paused = true;
+            };
+            assets = {
+                ledger = (Big_map.empty : ledger);
+                operators = (Big_map.empty : operator_storage);
+                token_total_supply = (Big_map.empty : token_total_supply);
+                token_metadata = (Big_map.empty : token_metadata_storage);
+            };
+        }
+        """
+            % self.admin_key.public_key_hash()
+        )
+
+        ptz_storage = ligo_mac.compile_storage(ligo_storage)
+        return ligo_mac.originate(self.util, ptz_storage)
+
+    def orig_inspector(self, ligo_inspector):
+        ligo_storage = "Empty unit"
+        ptz_storage = ligo_inspector.compile_storage(ligo_storage)
+        return ligo_inspector.originate(self.util, ptz_storage)
+
+    def orig_receiver(self, ligo_receiver):
+        return ligo_receiver.originate(self.util, balance=100000000)
+
+    def transfer_init_funds(self):
+        self.util.transfer(self.mike_key.public_key_hash(), 100000000)
+        self.util.transfer(self.kyle_key.public_key_hash(), 100000000)
+
+    def pause_fa2(self, paused: bool):
+        op = self.fa2.pause(paused).inject()
+        self.util.wait_for_ops(op)
+
+    def assertBalance(self, owner_address, token_id, expected_balance, msg=None):
+        op = self.inspector.query(
+            fa2=self.fa2.address, token_id=token_id, owner=owner_address
+        ).inject()
+        self.util.wait_for_ops(op)
+        b = self.inspector.storage()["state"]
+        print(b)
+        self.assertEqual(
+            {
+                "balance": expected_balance,
+                "request": {"token_id": token_id, "owner": owner_address},
+            },
+            b,
+            msg,
+        )
+
+    def create_token(self, id, symbol):
+        op = self.fa2.create_token(
+            token_id=id, symbol=symbol, name=symbol, decimals=0, extras={}
+        ).inject()
+        self.util.wait_for_ops(op)
+
+
+class TestMintBurn(TestMacSetUp):
+    def setUp(self):
+        super().setUp()
+        print("creating token TK1")
+        self.create_token(1, "TK1")
+        # needed to check balances which is guarded
+        print("unpausing")
+        self.pause_fa2(False)
+
+    def test_mint_burn_to_receiver(self):
+        self.mint_burn(self.alice_receiver.address)
+
+    def test_mint_burn_implicit(self):
+        self.mint_burn(self.mike_key.public_key_hash())
+
+    def mint_burn(self, owner_address):
+        print("minting")
+        mint_op = self.fa2.mint_tokens(
+            [{"owner": owner_address, "amount": 10, "token_id": 1}]
+        ).inject()
+        self.util.wait_for_ops(mint_op)
+        self.assertBalance(owner_address, 1, 10, "invalid mint balance")
+
+        print("burning")
+        burn_op = self.fa2.burn_tokens(
+            [{"owner": owner_address, "amount": 3, "token_id": 1}]
+        ).inject()
+        self.util.wait_for_ops(burn_op)
+
+        self.assertBalance(owner_address, 1, 7, "invalid balance after burn")
+
+
+class TestOperator(TestMacSetUp):
+    def setUp(self):
+        super().setUp()
+        self.pause_fa2(False)
+
+    def test_add_operator_to_receiver(self):
+
+        op_add = self.alice_receiver.owner_add_operator(
+            fa2=self.fa2.address, operator=self.admin_key.public_key_hash()
+        ).inject()
+        self.util.wait_for_ops(op_add)
+
+
+class TestTransfer(TestMacSetUp):
+    def setUp(self):
+        super().setUp()
+        self.pause_fa2(False)
+
+        op_op = self.alice_receiver.owner_add_operator(
+            fa2=self.fa2.address, operator=self.admin_key.public_key_hash()
+        ).inject()
+        self.util.wait_for_ops(op_op)
+        print("transfer test setup completed")
+
+    def test_transfer_to_receiver(self):
+        self.create_token(1, "TK1")
+        self.transfer(1, self.alice_receiver.address, self.bob_receiver.address)
+
+    def test_transfer_to_implicit(self):
+        self.create_token(2, "TK2")
+        self.transfer(2, self.alice_receiver.address, self.mike_key.public_key_hash())
+
+    def transfer(self, token_id, from_address, to_address):
+
+        mint_op = self.fa2.mint_tokens(
+            [{"owner": from_address, "amount": 10, "token_id": token_id}]
+        ).inject()
+        self.util.wait_for_ops(mint_op)
+
+        op_tx = self.fa2.transfer(
+            [
+                {
+                    "from_": from_address,
+                    "txs": [{"to_": to_address, "token_id": token_id, "amount": 3}],
+                }
+            ]
+        ).inject()
+        self.util.wait_for_ops(op_tx)
+
+        self.assertBalance(to_address, token_id, 3, "invalid recipient balance")
+        self.assertBalance(from_address, token_id, 7, "invalid source balance")
+
+    def test_batch_transfer(self):
+        self.create_token(1, "TK1")
+        self.create_token(2, "TK2")
+
+        alice_address = self.alice_receiver.address
+        bob_address = self.bob_receiver.address
+        mike_address = self.mike_key.public_key_hash()
+
+        mint_op = self.fa2.mint_tokens(
+            [
+                {"owner": alice_address, "amount": 10, "token_id": 1},
+                {"owner": alice_address, "amount": 10, "token_id": 2},
+            ]
+        ).inject()
+        self.util.wait_for_ops(mint_op)
+
+        op_tx = self.fa2.transfer(
+            [
+                {
+                    "from_": alice_address,
+                    "txs": [
+                        {"to_": bob_address, "token_id": 1, "amount": 3},
+                        {"to_": mike_address, "token_id": 2, "amount": 5},
+                    ],
+                }
+            ]
+        ).inject()
+        self.util.wait_for_ops(op_tx)
+
+        self.assertBalance(alice_address, 1, 7, "invalid TK1 reminder balance")
+        self.assertBalance(alice_address, 2, 5, "invalid TK1 reminder balance")
+        self.assertBalance(bob_address, 1, 3, "invalid TK1 received balance")
+        self.assertBalance(bob_address, 2, 0, "invalid TK2 received balance")
+        self.assertBalance(mike_address, 1, 0, "invalid TK1 received balance")
+        self.assertBalance(mike_address, 2, 5, "invalid TK2 received balance")
