249de75b234c67b26e0877a1703eb5b84d51bb33
Eugene Mishura <eugene@tqgroup.io>
2020-07-29 15:53:56 -0400

    setup test project for collectibles

---
+++ collectibles/test/collection.test.ts
@@ -0,0 +1,9 @@
+// import { bootstrap, TestTz } from './bootstrap-sandbox';
+import { $log } from '@tsed/logger';
+
+describe('hello', () => {
+  test('test', () => {
+    // $log.info('test');
+    console.debug('HELLO');
+  });
+});
---
+++ collectibles/test/jest.config.js
@@ -0,0 +1,1 @@
+../../shared/typescript/jest.config.js
