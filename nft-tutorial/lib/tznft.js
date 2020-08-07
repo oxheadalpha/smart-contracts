742317d5f71264eadfa4351e24191d50aafb83d5
Eugene Mishura <eugene@tqgroup.io>
2020-08-06 22:35:55 -0400

    flattened cli command description

--- nft-tutorial/lib/tznft.js
+++ nft-tutorial/lib/tznft.js
@@ -1,10 +1,52 @@
 #!/usr/bin/env node
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
-const cli = new commander_1.Command();
+const networkConf = __importStar(require("./config-network"));
+// configuration commands
 //prettier-ignore
-cli
-    .command('config', 'configure tznft').alias('cfg')
-    .command('originate', 'originate something')
-    .parse();
+commander_1.program
+    .command('config-init')
+    .alias('ci')
+    .description('creates tznft.config file')
+    .action(networkConf.initUserConfig);
+//prettier-ignore
+commander_1.program
+    .command('show-network')
+    .alias('shown')
+    .description('shows currently selected active network', { 'a': 'also shows all available networks' })
+    .option('-a --all', 'shows all available configured networks', false)
+    .action((options) => networkConf.showActiveNetwork(options.all))
+    .passCommandToAction(false);
+//prettier-ignore
+commander_1.program
+    .command('set-network')
+    .alias('setn')
+    .arguments('<network>')
+    .description('selected network to originate contracts')
+    .action(networkConf.setNetwork);
+//prettier-ignore
+commander_1.program
+    .command('config-show-all')
+    .description('shows whole raw config')
+    .action(networkConf.showConfig);
+commander_1.program.parse();
--- nft-tutorial/src/tznft.ts
+++ nft-tutorial/src/tznft.ts
@@ -1,10 +1,40 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { program } from 'commander';
+import * as networkConf from './config-network';
 
-const cli = new Command();
+// configuration commands
 
 //prettier-ignore
-cli
-  .command('config', 'configure tznft').alias('cfg')
-  .command('originate', 'originate something')
-  .parse();
+program
+  .command('config-init')
+  .alias('ci')
+  .description('creates tznft.config file')
+  .action(networkConf.initUserConfig);
+
+//prettier-ignore
+program
+  .command('show-network')
+  .alias('shown')
+  .description(
+    'shows currently selected active network', 
+    {'a': 'also shows all available networks'}
+  )
+  .option('-a --all', 'shows all available configured networks', false)
+  .action((options) => networkConf.showActiveNetwork(options.all))
+  .passCommandToAction(false)
+
+//prettier-ignore
+program
+  .command('set-network')
+  .alias('setn')
+  .arguments('<network>')
+  .description('selected network to originate contracts')
+  .action(networkConf.setNetwork)
+
+//prettier-ignore
+program
+  .command('config-show-all')
+  .description('shows whole raw config')
+  .action(networkConf.showConfig);
+
+program.parse();
