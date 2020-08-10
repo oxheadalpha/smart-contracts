55624a3a52600bac46093f63f220ba6bfa58c45f
Eugene Mishura <eugene@tqgroup.io>
2020-08-10 14:06:41 -0400

    nft tutorial: originating nft

--- nft-tutorial/src/contracts.ts
+++ nft-tutorial/src/contracts.ts
@@ -3,9 +3,10 @@ import * as kleur from 'kleur';
 import * as fs from 'fs';
 import * as path from 'path';
 import { BigNumber } from 'bignumber.js';
-import { TezosToolkit } from '@taquito/taquito';
+import { TezosToolkit, MichelsonMap } from '@taquito/taquito';
 import { InMemorySigner } from '@taquito/signer';
-import { getActiveNetworkCfg } from './config-util';
+import { getActiveNetworkCfg, loadUserConfig } from './config-util';
+import { resolveAlias2Signer } from './config-aliases';
 
 export interface Fa2TransferDestination {
   to_?: string;
@@ -29,6 +30,14 @@ export interface BalanceOfResponse {
 }
 
 type InspectorStorage = BalanceOfResponse[] | {};
+
+interface TokenMetadata {
+  token_id: BigNumber;
+  symbol: string;
+  name: string;
+  decimals: BigNumber;
+  extras: MichelsonMap<string, string>;
+}
 
 export function createToolkit(
   signer: InMemorySigner,
@@ -59,6 +68,57 @@ export async function originateInspector(tezos: TezosToolkit): Promise<string> {
   return originateContract(tezos, code, storage, 'inspector');
 }
 
+export async function mintNfts(
+  owner: string,
+  tokens: TokenMetadata[]
+): Promise<void> {
+  const config = loadUserConfig();
+  const signer = await resolveAlias2Signer(owner, config);
+  const ownerAddress = await signer.publicKeyHash();
+  const tz = createToolkit(signer, config);
+
+  const code = await loadFile(
+    path.join(__dirname, '../ligo/out/fa2_fixed_collection_token.tz')
+  );
+  const storage = createNftStorage(tokens, ownerAddress);
+
+  console.log(kleur.yellow('originating new NFT contract'));
+  const nftAddress = await originateContract(tz, code, storage, 'nft');
+  console.log(
+    kleur.yellow(`originated NFT collection ${kleur.green(nftAddress)}`)
+  );
+}
+
+export function parseTokens(
+  descriptor: string,
+  tokens: TokenMetadata[]
+): TokenMetadata[] {
+  const [id, symbol, name] = descriptor.split(',');
+  const token: TokenMetadata = {
+    token_id: new BigNumber(id),
+    symbol,
+    name,
+    decimals: new BigNumber(0),
+    extras: new MichelsonMap()
+  };
+  tokens.push(token);
+  return tokens;
+}
+
+function createNftStorage(tokens: TokenMetadata[], owner: string) {
+  const ledger = new MichelsonMap<BigNumber, string>();
+  const token_metadata = new MichelsonMap<BigNumber, TokenMetadata>();
+  for (let meta of tokens) {
+    ledger.set(meta.token_id, owner);
+    token_metadata.set(meta.token_id, meta);
+  }
+  return {
+    ledger,
+    operators: new MichelsonMap(),
+    token_metadata
+  };
+}
+
 async function loadFile(filePath: string): Promise<string> {
   return new Promise<string>((resolve, reject) =>
     fs.readFile(filePath, (err, buff) =>
@@ -70,14 +130,13 @@ async function loadFile(filePath: string): Promise<string> {
 async function originateContract(
   tz: TezosToolkit,
   code: string,
-  storage: any,
+  storage: string | object,
   name: string
 ): Promise<string> {
+  const origParam =
+    typeof storage === 'string' ? { code, init: storage } : { code, storage };
   try {
-    const originationOp = await tz.contract.originate({
-      code,
-      init: storage
-    });
+    const originationOp = await tz.contract.originate(origParam);
 
     const contract = await originationOp.contract();
     return contract.address;
