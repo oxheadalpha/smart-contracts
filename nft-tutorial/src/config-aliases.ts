0b8f74cbf78bebdd006b208bbdfe705a506bab4a
Eugene Mishura <eugene@tqgroup.io>
2020-08-09 20:35:19 -0400

    each configured alias has address and optional secret key

--- nft-tutorial/src/config-aliases.ts
+++ nft-tutorial/src/config-aliases.ts
@@ -1,4 +1,6 @@
 import * as kleur from 'kleur';
+import { validateAddress, ValidationResult } from '@taquito/utils';
+import { InMemorySigner } from '@taquito/signer';
 import { getActiveAliasesCfgKey, loadUserConfig } from './config-util';
 
 export function showAlias(alias: string): void {
@@ -7,35 +9,58 @@ export function showAlias(alias: string): void {
   if (alias) {
     const aliasKey = `${aliasesKey}.${alias}`;
     if (config.has(aliasKey)) {
-      const key_or_address = config.get(aliasKey);
-      console.log(kleur.yellow(`${alias}\t${key_or_address}`));
+      const aliasDef: any = config.get(aliasKey);
+      console.log(kleur.yellow(formatAlias(alias, aliasDef)));
     } else
       console.log(kleur.red(`alias ${kleur.yellow(alias)} is not configured`));
   } else {
     const allAliases = Object.getOwnPropertyNames(config.get(aliasesKey));
     for (let a of allAliases) {
       const aliasKey = `${aliasesKey}.${a}`;
-      const key_or_address = config.get(aliasKey);
-      console.log(kleur.yellow(`${a}\t${key_or_address}`));
+      const aliasDef: any = config.get(aliasKey);
+      console.log(kleur.yellow(formatAlias(a, aliasDef)));
     }
   }
 }
 
-export function addAlias(alias: string, key_or_address: string): void {
+function formatAlias(alias: string, def: any): string {
+  return `${alias}\t${def.address}\t${def.secret ? def.secret : ''}`;
+}
+
+export async function addAlias(
+  alias: string,
+  key_or_address: string
+): Promise<void> {
   const config = loadUserConfig();
   const aliasKey = `${getActiveAliasesCfgKey(config)}.${alias}`;
   if (config.has(aliasKey)) {
     console.log(kleur.red(`alias ${kleur.yellow(alias)} already exists`));
     return;
   }
-  if (!(key_or_address.startsWith('tz') || key_or_address.startsWith('edsk'))) {
-    console.log(
-      kleur.red('alias value can be either private key or tz address')
-    );
-    return;
-  }
+  const aliasDef = await validateKey(key_or_address);
+  if (!aliasDef) return;
 
-  config.set(aliasKey, key_or_address);
+  config.set(aliasKey, aliasDef);
+}
+
+interface AliasDef {
+  address: string;
+  secret?: string;
+}
+async function validateKey(
+  key_or_address: string
+): Promise<AliasDef | undefined> {
+  if (validateAddress(key_or_address) === ValidationResult.VALID)
+    return { address: key_or_address };
+  else
+    try {
+      const signer = await InMemorySigner.fromSecretKey(key_or_address);
+      const address = await signer.publicKeyHash();
+      return { address, secret: key_or_address };
+    } catch {
+      console.log(kleur.red('invalid address or secret key'));
+      return undefined;
+    }
 }
 
 export function removeAlias(alias: string): void {
