08f2df7447b6950aef214d6fa84072a800e7ca85
Eugene Mishura <eugene@tqgroup.io>
2020-10-07 12:59:00 -0400

    updated collection storage origination

--- collectibles/test/origination.ts
+++ collectibles/test/origination.ts
@@ -29,7 +29,7 @@ export async function originateCollection(
                 Elt 5 "${owner}" ;
                 Elt 6 "${owner}" }
               {})
-        (Pair (Pair (Pair None (Left (Right Unit))) (Pair (Left (Left Unit)) (Left (Right Unit))))
+        (Pair (Pair (Right (Right Unit)) (Pair (Right (Left Unit)) (Pair (Left Unit) None)))
               { Elt 0 (Pair 0 (Pair "RED" (Pair "RAINBOW_TOKEN" (Pair 0 {})))) ;
                 Elt 1 (Pair 1 (Pair "ORANGE" (Pair "RAINBOW_TOKEN" (Pair 0 {})))) ;
                 Elt 2 (Pair 2 (Pair "YELLOW" (Pair "RAINBOW_TOKEN" (Pair 0 {})))) ;
