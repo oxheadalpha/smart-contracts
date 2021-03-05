251e7b7353415cd69f50a7417fd8e1a27ada4f22
Eugene Mishura <eugene@tqgroup.io>
2021-03-05 15:29:39 -0500

    updated flextesa script to run edobox

--- shared/flextesa/start-sandbox.sh
+++ shared/flextesa/start-sandbox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm --name flextesa-sandbox -e block_time=5 --detach -p 20000:20000 tqtezos/flextesa:20200925 delphibox start
+docker run --rm --name flextesa-sandbox -e block_time=5 --detach -p 20000:20000 tqtezos/flextesa:20210216 edobox start
 
 
