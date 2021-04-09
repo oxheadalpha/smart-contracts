1823fa8ac76216e690dd6857b8149cf9a29a4c39
Eugene Mishura <eugene.mishura@tqtezos.com>
2021-04-09 17:27:40 -0400

    WIP: fractional dao voting with permit

--- generic_fractional_dao/src/lambdas.ts
+++ generic_fractional_dao/src/lambdas.ts
@@ -1,6 +1,13 @@
 import * as _ from "lodash";
 import { TezosToolkit } from "@taquito/taquito";
-import { Parser } from "@taquito/michel-codec";
+import { MichelsonV1Expression } from "@taquito/rpc";
+import {
+  Expr,
+  MichelsonData,
+  MichelsonType,
+  packData,
+  Parser,
+} from "@taquito/michel-codec";
 import { address, Contract, nat } from "smart-contracts-common/type-aliases";
 import { compileExpression, LigoEnv } from "smart-contracts-common/ligo";
 import { $log } from "@tsed/logger";
@@ -22,5 +29,54 @@ export const setDaoVotingThresholdParam = async (
     `set_dao_voting_threshold (${oldThreshold}n, ${newThreshold}n)`
   );
   const p = new Parser();
-  return p.parseMichelineExpression(lambdaMichelson);
+  const lambdaExp = p.parseMichelineExpression(lambdaMichelson);
+  if (!lambdaExp)
+    throw new Error(`Cannot parse lambda Michelson \n${lambdaMichelson}`);
+  return { lambdaExp, lambdaMichelson };
+};
+
+export const signPermit = async (
+  signer: TezosToolkit,
+  dao: Contract,
+  lambda: string
+) => {
+  const chainId = await signer.rpc.getChainId();
+  const { vote_count } = await dao.storage<DaoStorage>();
+  /*
+  Bytes.pack (
+    (Tezos.chain_id, Tezos.self_address),
+    (vote_count, lambda)
+  )
+  */
+
+  const michData = `
+  (Pair
+    (Pair
+      "${chainId}"
+      "${dao.address}"
+    )
+    (Pair
+      ${vote_count}
+      ${lambda}
+    )
+  )
+  `;
+
+  const michType = `
+  (pair
+    (pair chain_id address)
+    (pair nat (lambda unit (list operation)))
+  )
+  `;
+
+  const p = new Parser();
+  const dat = p.parseMichelineExpression(michData);
+  const typ = p.parseMichelineExpression(michType);
+  // const pack = packData(dat as MichelsonData, typ as MichelsonType);
+  const pack = await signer.rpc.packData({
+    data: dat as MichelsonV1Expression,
+    type: typ as MichelsonV1Expression,
+  });
+  const signature = await signer.signer.sign(pack.packed);
+  return signature;
 };
