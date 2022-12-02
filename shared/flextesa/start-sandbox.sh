fcfb83d0ec27f1abfd7d5471a09c8fb1b9644640
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2022-12-02 16:03:45 -0500

    run lima protocol on sandbox

--- shared/flextesa/start-sandbox.sh
+++ shared/flextesa/start-sandbox.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name flextesa-sandbox -e block_time=5  -e flextesa_node_cors_origin="*" --detach -p 20000:20000 oxheadalpha/flextesa:20220715 kathmandubox start
+docker run --rm --name flextesa-sandbox -e block_time=5  -e flextesa_node_cors_origin="*" --detach -p 20000:20000 oxheadalpha/flextesa:20221123 limabox start
