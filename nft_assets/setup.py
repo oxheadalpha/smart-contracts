283dba0e0c8c9e61f3b4db97f7ca08703902f182
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2022-12-02 16:52:08 -0500

    regression python tests

--- nft_assets/setup.py
+++ nft_assets/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="tezos_fa2_nft_tests",
-    version="1.3",
+    version="1.4",
     description="FA2 NFT Asset Contract Tests",
     packages=["tezos_fa2_nft_tests"],
     install_requires=["pysodium", "secp256k1", "fastecdsa", "pytezos", "crypto"],
