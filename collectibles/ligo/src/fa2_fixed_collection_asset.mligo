daa54ddc37d8e879b9e040451a2495db0c5bece2
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2023-10-13 14:30:14 -0400

    WIP: converting collectibles to Ligo 1.0

--- collectibles/ligo/src/fa2_fixed_collection_asset.mligo
+++ collectibles/ligo/src/fa2_fixed_collection_asset.mligo
@@ -1,27 +1,62 @@
 #include "fa2_fixed_collection_token.mligo"
 #include "../fa2_modules/simple_admin.mligo"
 
-type collection_asset_storage = {
-  assets : collection_storage;
-  admin : simple_admin_storage;
-  metadata : contract_metadata;
-}
+module CollectionAsset = struct
 
-type collection_asset_entrypoints = 
-  | Assets of fa2_entry_points
-  | Admin of simple_admin
+  type storage = {
+    assets : Token.storage;
+    admin : Admin.storage;
+    metadata : contract_metadata;
+  }
 
-let collection_asset_main (param, storage
-    : collection_asset_entrypoints * collection_asset_storage)
-    : (operation list) * collection_asset_storage =
-  match param with
-  | Assets p -> 
-    let _ = fail_if_paused storage.admin in
-    let ops, new_assets = fa2_collection_main (p, storage.assets) in
+  type return_type = operation list * storage
+
+  [@entry] let assets (p : fa2_entry_points) (storage : storage) : return_type =
+    let _ = Admin.fail_if_paused storage.admin in
+    let ops, new_assets = Token.fa2_main (p, storage.assets) in
     let new_s = { storage with assets = new_assets; } in
     (ops, new_s)
 
-  | Admin p ->
-    let ops, new_admin = simple_admin (p, storage.admin) in
+  [@entry] let admin (p : Admin.entrypoints) (storage : storage) : return_type =
+    let ops, new_admin = Admin.main (p, storage.admin) in
     let new_s = { storage with admin = new_admin; } in
     (ops, new_s)
+
+end
+
+(* this is a sample collection asset contract storage *)
+let store : CollectionAsset.storage = {
+  assets = {
+    ledger = Big_map.literal [
+      ( 0n, ("tz1YPSCGWXwBdTncK2aCctSZAXWvGsGwVJqU" : address))
+    ];
+    operators = (Big_map.empty : operator_storage);
+    token_metadata = Big_map.literal [
+                  ( 0n,
+                    {
+                      token_id = 0n;
+                      token_info = Map.literal [
+                        ("symbol", 0x544b31);
+                        ("name", 0x5465737420546f6b656e);
+                        ("decimals", 0x30);
+                      ];
+                    }
+                  ) 
+                ];
+    permissions = {
+      operator = Owner_or_operator_transfer;
+      receiver = Optional_owner_hook;
+      sender = Optional_owner_hook;
+      custom = (None : custom_permission_policy option);
+    };
+  };
+  admin = {
+    admin = ("tz1YPSCGWXwBdTncK2aCctSZAXWvGsGwVJqU" : address);
+    pending_admin = (None : address option);
+    paused = false;
+  };
+  metadata = Big_map.literal [
+    ("", Bytes.pack "tezos-storage:content" );
+    ("content", 0x00) (* bytes encoded UTF-8 JSON *)
+  ];
+}
--- collectibles/test/origination.ts
+++ collectibles/test/origination.ts
@@ -17,7 +17,7 @@ export async function originateCollection(
   const code = await compileAndLoadContract(
     env,
     'fa2_fixed_collection_asset.mligo',
-    'collection_asset_main',
+    'CollectionAsset',
     'fa2_fixed_collection_asset.tz'
   );
   const owner = await tz.signer.publicKeyHash();
@@ -33,46 +33,31 @@ export async function originateCollection(
   const meta_content = char2Bytes(JSON.stringify(meta, null, 2));
 
   const rainbow_storage = `
-  (Pair (Pair (Pair (Pair "${owner}" False) None)
-            (Pair (Pair { Elt 0 "${owner}" ;
-                          Elt 1 "${owner}" ;
-                          Elt 2 "${owner}" ;
-                          Elt 3 "${owner}" ;
-                          Elt 4 "${owner}" ;
-                          Elt 5 "${owner}" ;
-                          Elt 6 "${owner}" }
-                        {})
-                  (Pair (Pair (Right (Right Unit)) (Pair (Right (Left Unit)) (Pair (Left Unit) None)))
-                        { Elt 0 (Pair 0 ${token_meta_literal(
-                          'RED',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 1 (Pair 1 ${token_meta_literal(
-                          'ORANGE',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 2 (Pair 2 ${token_meta_literal(
-                          'YELLOW',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 3 (Pair 3 ${token_meta_literal(
-                          'GREEN',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 4 (Pair 4 ${token_meta_literal(
-                          'BLUE',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 5 (Pair 5 ${token_meta_literal(
-                          'INDIGO',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 6 (Pair 6 ${token_meta_literal(
-                          'VIOLET',
-                          'RAINBOW_TOKEN'
-                        )}) 
-                        })))
-      { Elt "" 0x${meta_uri} ; Elt "content" 0x${meta_content} })
+(Pair (Pair { 
+  Elt 0 "${owner}" ;
+  Elt 1 "${owner}" ;
+  Elt 2 "${owner}" ;
+  Elt 3 "${owner}" ;
+  Elt 4 "${owner}" ;
+  Elt 5 "${owner}" ;
+  Elt 6 "${owner}" 
+      }
+{}
+{ 
+  Elt 0 (Pair 0 ${token_meta_literal('RED', 'RAINBOW_TOKEN')}) ;
+  Elt 1 (Pair 1 ${token_meta_literal( 'ORANGE', 'RAINBOW_TOKEN')}) ;
+  Elt 2 (Pair 2 ${token_meta_literal('YELLOW', 'RAINBOW_TOKEN')}) ;
+  Elt 3 (Pair 3 ${token_meta_literal('GREEN', 'RAINBOW_TOKEN')}) ;
+  Elt 4 (Pair 4 ${token_meta_literal('BLUE', 'RAINBOW_TOKEN')}) ;
+  Elt 5 (Pair 5 ${token_meta_literal('INDIGO', 'RAINBOW_TOKEN')}) ;
+  Elt 6 (Pair 6 ${token_meta_literal('VIOLET', 'RAINBOW_TOKEN')})
+}
+(Right (Right Unit))
+(Right (Left Unit))
+(Right (Left Unit))
+None)
+(Pair "${owner}" None False)
+{ Elt "" 0x${meta_uri} ; Elt "content" 0x${meta_content} })
   `;
   return originateContract(tz, code, rainbow_storage, 'collection');
 }
@@ -84,7 +69,7 @@ export async function originateMoney(
   const code = await compileAndLoadContract(
     env,
     'fa2_single_asset_with_hooks.mligo',
-    'single_asset_main',
+    'SingleAsset',
     'fa2_single_asset_with_hooks.tz'
   );
   const owner = await tz.signer.publicKeyHash();
@@ -130,7 +115,7 @@ export async function originatePromo(
   const code = await compileAndLoadContract(
     env,
     'collectibles_promo.mligo',
-    'main',
+    'CollectiblesPromo',
     'collectibles_promo.tz'
   );
   const owner = await tz.signer.publicKeyHash();
