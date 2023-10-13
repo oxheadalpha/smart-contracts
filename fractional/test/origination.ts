efaa4b9fdd075b50fa797cbcb99d63a0fab2d7c3
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2023-10-13 15:40:20 -0400

    convert fractional dao contract to Ligo 1.0

--- fractional/test/origination.ts
+++ fractional/test/origination.ts
@@ -15,7 +15,7 @@ export async function originateNftCollection(
   const code = await compileAndLoadContract(
     env,
     'fa2_fixed_collection_asset.mligo',
-    'collection_asset_main',
+    'CollectionAsset',
     'fa2_fixed_collection_asset.tz'
   );
   const owner = await tz.signer.publicKeyHash();
@@ -31,46 +31,31 @@ export async function originateNftCollection(
   const meta_content = char2Bytes(JSON.stringify(meta, null, 2));
 
   const rainbow_storage = `
-  (Pair (Pair (Pair (Pair "${owner}" False) None)
-            (Pair (Pair { Elt 0 "${owner}" ;
-                          Elt 1 "${owner}" ;
-                          Elt 2 "${owner}" ;
-                          Elt 3 "${owner}" ;
-                          Elt 4 "${owner}" ;
-                          Elt 5 "${owner}" ;
-                          Elt 6 "${owner}" }
-                        {})
-                  (Pair (Pair (Right (Right Unit)) (Pair (Right (Left Unit)) (Pair (Left Unit) None)))
-                        { Elt 0 (Pair 0 ${token_meta_literal(
-                          'RED',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 1 (Pair 1 ${token_meta_literal(
-                          'ORANGE',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 2 (Pair 2 ${token_meta_literal(
-                          'YELLOW',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 3 (Pair 3 ${token_meta_literal(
-                          'GREEN',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 4 (Pair 4 ${token_meta_literal(
-                          'BLUE',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 5 (Pair 5 ${token_meta_literal(
-                          'INDIGO',
-                          'RAINBOW_TOKEN'
-                        )}) ;
-                        Elt 6 (Pair 6 ${token_meta_literal(
-                          'VIOLET',
-                          'RAINBOW_TOKEN'
-                        )}) 
-                        })))
-      { Elt "" 0x${meta_uri} ; Elt "content" 0x${meta_content} })
+(Pair (Pair { 
+  Elt 0 "${owner}" ;
+  Elt 1 "${owner}" ;
+  Elt 2 "${owner}" ;
+  Elt 3 "${owner}" ;
+  Elt 4 "${owner}" ;
+  Elt 5 "${owner}" ;
+  Elt 6 "${owner}" 
+      }
+{}
+{ 
+  Elt 0 (Pair 0 ${token_meta_literal('RED', 'RAINBOW_TOKEN')}) ;
+  Elt 1 (Pair 1 ${token_meta_literal( 'ORANGE', 'RAINBOW_TOKEN')}) ;
+  Elt 2 (Pair 2 ${token_meta_literal('YELLOW', 'RAINBOW_TOKEN')}) ;
+  Elt 3 (Pair 3 ${token_meta_literal('GREEN', 'RAINBOW_TOKEN')}) ;
+  Elt 4 (Pair 4 ${token_meta_literal('BLUE', 'RAINBOW_TOKEN')}) ;
+  Elt 5 (Pair 5 ${token_meta_literal('INDIGO', 'RAINBOW_TOKEN')}) ;
+  Elt 6 (Pair 6 ${token_meta_literal('VIOLET', 'RAINBOW_TOKEN')})
+}
+(Right (Right Unit))
+(Right (Left Unit))
+(Right (Left Unit))
+None)
+(Pair "${owner}" None False)
+{ Elt "" 0x${meta_uri} ; Elt "content" 0x${meta_content} })
   `;
   return originateContract(tz, code, rainbow_storage, 'NFT collection');
 }
@@ -82,7 +67,7 @@ export async function originateFractionalDao(
   const code = await compileAndLoadContract(
     env,
     'fractional_dao.mligo',
-    'dao_main',
+    'FractionalDao',
     'fractional_dao.tz'
   );
   const admin = await tz.signer.publicKeyHash();
@@ -99,11 +84,14 @@ export async function originateFractionalDao(
   const meta_content = char2Bytes(JSON.stringify(meta, null, 2));
 
   const storage = `
-  (Pair (Pair (Pair (Pair (Pair "${admin}" False) None)
-                  { Elt "" 0x${meta_uri} ;
-                    Elt "content" 0x${meta_content} })
-            (Pair 0 {}))
-      (Pair (Pair (Pair (Pair {} {}) (Pair {} {})) {}) 0))
+  (Pair (Pair {} {} {} {})
+  0
+  (Pair "${admin}" None False)
+  0
+  {}
+  {}
+  { Elt "" 0x${meta_uri} ;
+    Elt "content" 0x${meta_content} })
   `;
 
   return originateContract(tz, code, storage, 'fractional DAO');
